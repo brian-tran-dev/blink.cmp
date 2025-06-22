04d5befcfd9f0bfd68fe5255c7f82fa5651699ec
Brian Tran <khoaphananhtran@gmail.com>
2025-06-23 02:13:57 +0700

    add signature scrolling

--- lua/blink/cmp/config/keymap.lua
+++ lua/blink/cmp/config/keymap.lua
@@ -19,6 +19,8 @@
 --- | 'scroll_documentation_down' Scroll the documentation window down
 --- | 'show_signature' Show the signature help window
 --- | 'hide_signature' Hide the signature help window
+--- | 'scroll_signature_up' Scroll the documentation window up
+--- | 'scroll_signature_down' Scroll the documentation window down
 --- | 'snippet_forward' Move the cursor forward to the next snippet placeholder
 --- | 'snippet_backward' Move the cursor backward to the previous snippet placeholder
 --- | (fun(cmp: blink.cmp.API): boolean?) Custom function where returning true will prevent the next command from running
